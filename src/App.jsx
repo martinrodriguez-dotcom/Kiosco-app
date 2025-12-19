c94917ae5b3116694e3af9ac1feedfba597cd1f7
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2025-12-18 22:58:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-// Asegúrate de que la ruta coincida con donde guardaste el formulario
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+import ProductList from './modules/products/ProductList';
 import AddProductForm from './modules/products/AddProductForm';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-center my-4">Sistema de Gestión Kiosco</h1>
-      {/* Aquí mostramos el componente que creamos antes */}
-      <AddProductForm />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* El Layout envuelve a todas las rutas internas */}
+        <Route path="/" element={<Layout />}>
+          
+          {/* Ruta principal: Lista de productos */}
+          <Route index element={<ProductList />} />
+          
+          {/* Ruta: Cargar nuevo producto */}
+          <Route path="nuevo-producto" element={<AddProductForm />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
