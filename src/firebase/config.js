7246fa0a663381801f67d41ab95bfbb9ddc57723
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2025-12-18 22:25:22 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // Base de datos
+import { getAuth } from "firebase/auth";           // Autenticación (Login)
 
-// Pega aquí tu configuración de Firebase Console
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDfniZVLGzatksiK1qBeO259XqpY46PbX0",
+  authDomain: "kiosco-app-79b58.firebaseapp.com",
+  projectId: "kiosco-app-79b58",
+  storageBucket: "kiosco-app-79b58.firebasestorage.app",
+  messagingSenderId: "610299820416",
+  appId: "1:610299820416:web:a643e728ba5ee6d7a7e965"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializamos y exportamos los servicios que usaremos en la app
 export const db = getFirestore(app);
 export const auth = getAuth(app);
