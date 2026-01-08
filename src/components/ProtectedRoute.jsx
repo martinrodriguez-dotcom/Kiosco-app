10570dbddd108b02ac39bc0d5ebb33f8013b23e5
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-01-08 15:46:58 -0300

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <div className="p-10 text-center">Cargando...</div>;
+  
+  if (!user) return <Navigate to="/login" />;
+
+  return children;
+};
