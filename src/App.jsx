7069c33916f4958ae3539e3a59cdef3ecb1e9467
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-01-09 12:23:36 -0300

    Add CashboxHistoryList to App component routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 import Layout from './layout/Layout';
 import Login from './modules/auth/Login';
 
-// Módulos
+// Módulos de Productos y Ventas
 import ShopDashboard from './modules/products/ShopDashboard';
 import AddProductForm from './modules/products/AddProductForm';
 import RestockForm from './modules/products/RestockForm';
 import EditProductForm from './modules/products/EditProductForm';
+import CashboxHistoryList from './modules/products/CashboxHistoryList'; // <--- NUEVO IMPORT
+
+// Módulos de Proveedores
 import SupplierPaymentForm from './modules/products/SupplierPaymentForm';
 import SupplierAccount from './modules/products/SupplierAccount';
 
@@ -29,14 +32,20 @@ function App() {
               <Layout />
             </ProtectedRoute>
           }>
+            {/* Ruta Index: Dashboard (POS y Caja Actual) */}
             <Route index element={<ShopDashboard />} />
             
+            {/* Gestión de Productos */}
             <Route path="nuevo-producto" element={<AddProductForm />} />
             <Route path="reponer-stock" element={<RestockForm />} />
             <Route path="editar-producto/:id" element={<EditProductForm />} />
             
+            {/* Gestión de Proveedores */}
             <Route path="pago-proveedores" element={<SupplierPaymentForm />} />
             <Route path="cuenta-proveedores" element={<SupplierAccount />} />
+            
+            {/* Gestión de Cajas */}
+            <Route path="historial-cajas" element={<CashboxHistoryList />} />
           </Route>
         </Routes>
       </BrowserRouter>
